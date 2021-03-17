96a0af152c530eef148af291d4422fba7a2e0389
Dennis Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2021-03-17 02:07:55 +0000

    Update constants.dart

--- shared_utils/lib/src/constants.dart
+++ shared_utils/lib/src/constants.dart
@@ -3,11 +3,6 @@ library shared_utils;
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-/// text
-const kAppName = 'Q-Rentals';
-const kAppNameLong = 'Home & Car rental';
-const kAppDescription = 'Largest rental service in Accra';
-
 /// fonts
 TextTheme kPrimaryFonts = GoogleFonts.poppinsTextTheme();
 TextTheme kSecondaryFonts = GoogleFonts.rubikTextTheme();
