0ad308e3c9b0781c1e774008ae7ac8ccc35d7376
Dennis Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2021-03-20 22:45:54 +0000

    Added bloc

---
+++ shared_utils/lib/src/bloc/base_bloc.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+
+import 'bloc_state.dart';
+
+/// base class for all blocs -> provides logging events
+abstract class BaseBloc<E> extends Bloc<E, BlocState> {
+  BaseBloc() : super(BlocState.initialState());
+}
